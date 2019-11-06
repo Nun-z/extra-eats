1f8ec20d60595249d0f5f11c1821edcf69fd2eb2
Nick Witkin <nickwitkin@gmail.com>
2019-11-05 17:36:57 -0800

    Fixed uploading images to Cloudinary for posts.

--- src/upload.js
+++ src/upload.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary');
 const cloudinaryStorage = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.MY_CLOUD_NAME,
-  api_key: process.env.MY_CLOUD_API_KEY,
-  api_secret: process.env.MY_CLOUD_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
 });
 
 var storage = cloudinaryStorage({
