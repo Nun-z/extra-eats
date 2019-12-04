d62bbea2f8aac46faee6a60f53819be8662c2c08
Nick <nickwitkin@gmail.com>
2019-12-04 18:45:10 -0500

    Changed the iOS provisioning profile.

---
+++ client/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Nick/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Nick/Desktop/ExtraEats/extra-eats/client"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Nick/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.0.2"
+export "FLUTTER_BUILD_NUMBER=1"
